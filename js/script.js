af27126ff75abfebfd2f1d186d89b8419d8f5059
michael carlson <mtcarlso@gmail.com>
2014-03-08 20:58:37 -0500

    First javascripts; change the icon color when click on color box

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+
+//variables for colors are not strings; will need to create variables
+
+var tie {
+	width: "skinny";
+	color1: blue;
+	color2: green;
+	color3: red;
+	tipping: gray;
+	initials: "TF";
+};
