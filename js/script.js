730bf7700d6434adfd56b123b5154548ced153ff
michael carlson <mtcarlso@gmail.com>
2014-03-09 15:33:26 -0400

    saving variables from width and color1 into tie object

--- js/script.js
+++ js/script.js
@@ -3,11 +3,3 @@
 
 //variables for colors are not strings; will need to create variables
 
-var tie {
-	width: "skinny";
-	color1: blue;
-	color2: green;
-	color3: red;
-	tipping: gray;
-	initials: "TF";
-};
