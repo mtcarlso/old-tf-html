0b4066dfc50605fef27a82103f1de679596fbfc8
michael carlson <mtcarlso@gmail.com>
2014-03-09 19:51:04 -0400

    scroll to section on link clink; full screen background at any adjustment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//our tie, in use
 var tie = {
 		width: "skinny",
 		color1: "navy",
@@ -6,38 +7,56 @@ var tie = {
 		tipping: "gray",
 		initials: "TF"
 	};
-  $("[data-width]").click( function() {
-    tie.width = $(this).data("width");
-  })
 
-	$(".color-box").click( function() {
-		//this variable in this context might be best if it came from a centralized object for the tie
-    var chosenColor = $(this).data("color");
-		
-    var targetId = $("#base-color");
-		var multi = $("#color").css('background-image').split(',');
-    var bgImages = "";
-    
-    //adds and removes class from the big box to show selected color
-    targetId.removeClass(targetId.attr('class').split(' ').pop());
-		targetId.addClass(chosenColor);
-    targetId.data("color", chosenColor);
-    tie.color1 = chosenColor;
-		
-    //assigns the appropriate background image to the right spot in the array; hardcoded to be the first one, needs to be abstracted to work for all
-    multi[0] = "url(img/Color_1_" + chosenColor + ".png)";
-    bgImages = multi[0];
-    for (var i=1; i<multi.length;i++)
-    {
-      var bgImages = bgImages + "," + multi[i];
-    };
-    $("#color").css('background-image', bgImages);
-		return false;
-	});
-  $(".color-box").blur( function() {
-  	//save the value of the color box to the tie data object
-  });
+//animate to sections of the site
+$('.scene_link').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
 
-  $("#monogram-text").blur( function() {
-    tie.initials = $(this).val();
-  });
+
+//resize sections to be full height of browser; needs testing on giant monitors, maybe some max and min conditions
+$(function(){
+    $('.customizer').css({'height':($(window).height())+'px'});
+    $(window).resize(function(){
+        $('.customizer').css({'height':($(window).height())+'px'});
+    });
+});
+
+//save tie width on user click
+$("[data-width]").click( function() {
+  tie.width = $(this).data("width");
+})
+
+//update background image, big color box to change on click of color selection for the first color
+$(".color-box").click( function() {
+	//this variable in this context might be best if it came from a centralized object for the tie
+  var chosenColor = $(this).data("color");
+	
+  var targetId = $("#base-color");
+	var multi = $("#color").css('background-image').split(',');
+  var bgImages = "";
+  
+  //adds and removes class from the big box to show selected color
+  targetId.removeClass(targetId.attr('class').split(' ').pop());
+	targetId.addClass(chosenColor);
+  targetId.data("color", chosenColor);
+  tie.color1 = chosenColor;
+	
+  //assigns the appropriate background image to the right spot in the array; hardcoded to be the first one, needs to be abstracted to work for all
+  multi[0] = "url(img/Color_1_" + chosenColor + ".png)";
+  bgImages = multi[0];
+  for (var i=1; i<multi.length;i++)
+  {
+    var bgImages = bgImages + "," + multi[i];
+  };
+  $("#color").css('background-image', bgImages);
+	return false;
+});
+
+//save the initials for monogram
+$("#monogram-text").blur( function() {
+  tie.initials = $(this).val();
+});
